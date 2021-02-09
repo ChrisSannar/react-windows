88598dcb1c6e3ce14c0f3adbb0e73abaf2dd3aa4
Christopher Sannar <chris.sannar.dev@gmail.com>
2021-02-08 23:13:20 -0700

    Window Resizing
    Got it all to work, this is the flow of actions: 1. Is the mouse near an edge? 2. Which edge is it nearest to? 3. Mouse down? 4. Resize based on where the mouse is. I know it's getting a bit more crowded, but we're getting it running first, and then we'll clean it up.

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -7,32 +7,40 @@ export interface WindowProps {
   height?: string
 }
 
+const BORDER_MARGIN = 5 // How close we have to be in pixels to the window edge before we activate whichever function
+
 const Window: React.FC<WindowProps> = (props) => {
-  const { width, height} = props;
-  
+  const { width, height } = props
+
   // Using the ref makes a app refresh smoother as oppose to updating the styling using the state
-  const window = useRef<HTMLDivElement>(null);
+  const window = useRef<HTMLDivElement>(null)
 
   // We use the app components mouse information to make decisions here
   const [mouseX, mouseY] = useContext(MouseContext)
 
   // All the draging state information we need
   const [dragging, setDragging] = useState(false)
-  const [mouseBoxPosition, setMouseBoxPosition] = useState([0, 0])  // Where the cursor clicks on the header
+  const [mouseBoxPosition, setMouseBoxPosition] = useState([0, 0]) // Where the cursor clicks on the header
+
+  // All the resizing state information we need
+  const [resizing, setResizing] = useState(false)
+  const [edge, setEdge] = useState('')
 
   // The default styling for the window
-  const [windowStyling] = useState({
-    width: (width ?? "0") + "px",
-    height: (height ?? "0") + "px",
+  const [windowStyling, setWindowStyling] = useState({
+    width: (width ?? '0') + 'px',
+    height: (height ?? '0') + 'px',
+    cursor: 'auto',
   })
 
   /*
     Dragging
   */
   // When we click on the header, start dragging it
-  const handleHeaderMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+  const handleHeaderMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ): void => {
     if (window.current) {
-
       // Make sure we've got the exact position of where we clicked on the header and then start dragging
       const rect = window.current.getBoundingClientRect()
       setMouseBoxPosition([event.clientX - rect.x, event.clientY - rect.y])
@@ -48,13 +56,12 @@ const Window: React.FC<WindowProps> = (props) => {
   // Move the box to the given cordinates (such cordinates are typically the mouse)
   const moveWindowToMouse = (x: number, y: number): void => {
     if (window.current) {
-
       // We want to move the box relative to where we clicked it (keep the cursor in the same spot on the box)
-      const [mouseBoxX, mouseBoxY] = mouseBoxPosition;
+      const [mouseBoxX, mouseBoxY] = mouseBoxPosition
       const left = x - mouseBoxX
       const top = y - mouseBoxY
-      window.current.style.left = left + "px"
-      window.current.style.top = top + "px"
+      window.current.style.left = left + 'px'
+      window.current.style.top = top + 'px'
     }
   }
 
@@ -65,19 +72,148 @@ const Window: React.FC<WindowProps> = (props) => {
       moveWindowToMouse(mouseX, mouseY)
       return
     }
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, [mouseX, mouseY])
 
-  return <div 
-    className="Window"
-    ref={window}
-    style={{ ...windowStyling }}
+  /*
+    Resizing
+  */
+  // Lets us know if x/y are within the BORDER_MARGIN of the window edge
+  const nearWindow = (x: number, y: number): boolean => {
+    if (window.current) {
+      const rect = window.current.getBoundingClientRect()
+      return (
+        x > rect.left - BORDER_MARGIN &&
+        x < rect.right + BORDER_MARGIN &&
+        y > rect.top - BORDER_MARGIN &&
+        y < rect.bottom + BORDER_MARGIN
+      )
+    } else {
+      return false
+    }
+  }
+
+  // Returns which edge we're closest to
+  const getEdgeWithinMargin = (x: number, y: number): string => {
+    let result = ''
+    if (nearWindow(x, y) && window.current) {
+      const rect = window.current.getBoundingClientRect()
+      // if (Math.abs(y - rect.top) < BORDER_MARGIN) {
+      //   result += "top"
+      // }
+      if (Math.abs(y - rect.bottom) < BORDER_MARGIN) {
+        result += 'bottom'
+      }
+      if (Math.abs(x - rect.left) < BORDER_MARGIN) {
+        result += 'left'
+      }
+      if (Math.abs(x - rect.right) < BORDER_MARGIN) {
+        result += 'right'
+      }
+    }
+    return result
+  }
+
+  // Sets the cursor and the current resize configuration
+  const setCursorToEdge = (edge: string) => {
+    switch (edge) {
+      case 'top':
+      case 'bottom':
+        setWindowStyling({ ...windowStyling, cursor: 'ns-resize' })
+        break
+      case 'left':
+      case 'right':
+        setWindowStyling({ ...windowStyling, cursor: 'ew-resize' })
+        break
+      case 'bottomleft':
+      case 'topright':
+        setWindowStyling({ ...windowStyling, cursor: 'nesw-resize' })
+        break
+      case 'topleft':
+      case 'bottomright':
+        setWindowStyling({ ...windowStyling, cursor: 'nwse-resize' })
+        break
+      default:
+        setWindowStyling({ ...windowStyling, cursor: 'auto' })
+        break
+    }
+  }
+
+  // resizes the window based on were the mouse is
+  const resizeWindowToMouse = (x: number, y: number) => {
+    if (window.current) {
+      const {
+        width,
+        height,
+        bottom,
+        left,
+        right,
+      } = window.current.getBoundingClientRect()
+
+      // What edge are we resizing?
+      switch (edge) {
+        case 'bottom':
+          // Set the new height to itself minus the new difference (plus 1 to keep up the cursor)
+          window.current.style.height = height - (bottom - y) + 1 + 'px'
+          break
+        case 'right':
+          // Same idea here
+          window.current.style.width = width - (right - x) + 1 + 'px'
+          break
+        case 'bottomright':
+          // Run both values in the case of a corner
+          window.current.style.height = height - (bottom - y) + 1 + 'px'
+          window.current.style.width = width - (right - x) + 1 + 'px'
+          break
+        case 'left':
+          // With the left, we have to shift it over as well
+          window.current.style.width = width + (left - x) + 'px'
+          window.current.style.left = x + 'px'
+          break
+        case 'bottomleft':
+          window.current.style.height = height - (bottom - y) + 1 + 'px'
+          window.current.style.width = width + (left - x) + 'px'
+          window.current.style.left = x + 'px'
+          break
+      }
+    }
+  }
+
+  // Same deal with dragging, we're using a LayoutEffect to change the box position
+  useLayoutEffect(() => {
+    // Same as the dragging, if we're in a 'resizing' state, the move the mouse accordingly
+    if (resizing) {
+      resizeWindowToMouse(mouseX, mouseY)
+    }
+
+    // Set the cursor when we're just hovering over it
+    const edgey = getEdgeWithinMargin(mouseX, mouseY)
+    if (edgey) {
+      setEdge(edgey)
+      setCursorToEdge(edgey)
+    } else {
+      setEdge('')
+      setWindowStyling({ ...windowStyling, cursor: 'auto' })
+    }
+
+    // eslint-disable-next-line
+  }, [mouseX, mouseY])
+
+  return (
+    <div
+      className="Window"
+      ref={window}
+      style={{ ...windowStyling }}
+      onMouseDown={() => setResizing(!!edge)}
+      onMouseUp={() => setResizing(false)}
     >
-      <div 
-      className="WindowHeader"
-      onMouseDown={handleHeaderMouseDown}
-      onMouseUp={handleHeaderMouseUp}></div>
-  </div>
+      <div
+        className="WindowHeader"
+        onMouseDown={handleHeaderMouseDown}
+        onMouseUp={handleHeaderMouseUp}
+      ></div>
+    </div>
+  )
 }
 
 export default Window
